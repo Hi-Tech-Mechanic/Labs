b7b0961f4f7fbbfb557b297715d7c5a42ff4bad8
Hi-Tech-Mechanic <157356548+Hi-Tech-Mechanic@users.noreply.github.com>
2024-01-28 22:22:09 +0300

    Add files via upload

---
+++ Lab4/DateTime_Methods.py
@@ -0,0 +1,90 @@
+﻿# Программирование на языке высокого уровня (Python).
+# Задание №5 Вариант !!!
+#
+# Выполнил: Минатуллаев Р.Ш.
+# Группа: ПИН-б-з-21-1
+# E-mail: ruslan.minatullaev-1000@yandex.ru
+
+from datetime import datetime
+import json
+
+def decorator(func):
+    #return '<decorator>', func, '</decorator>'
+    decor = "{}{}".format(("#"), func) #"#"*20),"\n"
+    return decor
+
+
+class DateTime:
+    def __init__(self, date, time):
+        self.__date = date;
+        self.__time = time;
+
+    def __str__(self):
+        message = "Дата:{}\t||\tВремя:{}".format(str(self.__date), str(self.__time))
+        return message
+
+    def __add__(self, other):
+        return ("$"*100)
+
+    def current_datetime(self):
+        print("Текущее время:", datetime.now())
+
+    #@decorator
+    def get_date(self):
+        return ([element for element in self.__date]);
+
+    #@decorator
+    def get_time(self):
+        return ([element for element in self.__time]);
+
+    def set_date(self, value):
+        self.__date = value;
+
+    def set_time(self, value):
+        self.__time = value;
+
+    def display_all_attributes(self):
+        print("Дата:", self.get_date())
+        print("Время:", self.get_time())
+
+    def save(self):
+        data = {"time": self.__time, "date": self.__date}
+        with open("data.json", "w") as file:
+            json.dump(data, file)
+        print("Данные сохранены:", self.get_time(), "\t", self.get_date())
+
+    def load(self):
+        with open("data.json", "r") as file:
+            json.load(file)
+        print("Данные загружены:", self.get_time(), "\t", self.get_date())
+
+class MoonTime(DateTime):
+    def __init__(self, time, date):
+        self.__time = time
+        self.__date = date
+
+    def __str__(self):
+        return("{indent}Дата и время на луне:\n{indent_2}Дата:{date}\tВремя:{time}"
+               .format(date = self.get_time(), time = self.get_date(), indent="\t"*3, indent_2="\t"*2))
+
+    def get_time(self):
+        return [round(element / 2, 2) for element in self.__time]
+
+    def get_date(self):
+        return [round(element / 2, 2) for element in self.__date]
+
+class open_sample_class(DateTime):
+    def __init__(self, value):
+        self.field = value
+        
+    def __sub__(self, other):
+        return ("{} - вычитание".format(self.field - other.field))
+
+    def __mul__(self, other):
+        return ("{} - умножение".format(self.field * other.field))
+
+    def __floordiv__(self, other):
+        return ("{} - деление".format(self.field / other.field))
+
+    def __truediv__(self, other):
+        return ("{} - деление на цело".format(self.field // other.field))
--- Lab4/main.py
+++ Lab4/main.py
@@ -1,1 +1,40 @@
+﻿# Программирование на языке высокого уровня (Python).
+# Задание №5 Вариант !!!
+#
+# Выполнил: Минатуллаев Р.Ш.
+# Группа: ПИН-б-з-21-1
+# E-mail: ruslan.minatullaev-1000@yandex.ru
 
+from DateTime_Methods import DateTime
+from DateTime_Methods import MoonTime
+from DateTime_Methods import open_sample_class
+
+###################################################
+obj = DateTime([2024, 1, 24], [13, 58])
+#get_date = decorator(obj.get_date)
+
+"Вывод и работа с методами"
+obj.display_all_attributes()
+obj.current_datetime()
+obj.set_date([2077, 1, 24])
+obj.set_time([10, 15])
+obj.display_all_attributes()
+print(obj)
+
+"Сохранение и загрузка"
+obj.save()
+obj.load()
+###################################################
+obj_2 = MoonTime(obj.get_time(), obj.get_date())
+obj_3 = obj_2
+print("\n"*3,obj_2)
+
+"Перерузка операторов"
+tmp_1 = open_sample_class(10)
+tmp_2 = open_sample_class(50)
+print(tmp_1 + tmp_2)
+print(tmp_1 - tmp_2)
+print(tmp_1 * tmp_2)
+print(tmp_1 / tmp_2)
+print(tmp_1 // tmp_2)
+###################################################
