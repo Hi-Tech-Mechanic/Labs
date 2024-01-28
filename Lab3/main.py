5ae88754955a1e00b1eede5558ac209e32fdf836
Hi-Tech-Mechanic <157356548+Hi-Tech-Mechanic@users.noreply.github.com>
2024-01-28 22:20:39 +0300

    Add files via upload

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,1 +1,70 @@
+﻿# Программирование на языке высокого уровня (Python).
+# Задание №______. Вариант !!!
+#
+# Выполнил: Минатуллаев Р.Ш.
+# Группа: ПИН-б-з-21-1
+# E-mail: ruslan.minatullaev-1000@yandex.ru
 
+from deposit import deposits
+import time
+
+if __name__ == "__main__":
+    print("Добро пожаловать в систему подбора вкладов!")
+
+    while True:
+        print("\n-----")
+        print("Нажмите 1, чтобы подобрать вклад, или что угодно для выхода.")
+
+        answer = input()
+        if answer == "1":
+
+            initial_sum = float(input("1/2: Введите начальную сумму вклада: "))
+            period = int(input("2/2: Введите срок вклада (мес.): "))
+
+            matched_deposits = []
+            for deposit in deposits:
+                try:
+                    deposit._check_user_params(initial_sum, period)
+                    matched_deposits.append(deposit)
+                except AssertionError as err:
+                    pass
+
+            if len(matched_deposits) > 0:
+                print("Доступные вклады")
+                for deposit in matched_deposits:
+                    time.sleep(1)
+                    print(deposit)
+
+            if len(matched_deposits) > 0:
+                print("{0:18} | {1:13} | {2:13}".format(
+                    "Вклад", "Прибыль", "Итоговая сумма"
+                ))
+                for deposit in matched_deposits:
+                    time.sleep(0.5)
+                    print("{0:18} | {1:8,.2f} {3:4} | {2:8,.2f} {3:4}".format(
+                          deposit.name,
+                          deposit.get_profit(initial_sum, period),
+                          deposit.get_sum(initial_sum, period),
+                          deposit.currency))
+            else:
+                print("К сожалению, нет подходящих Вам вкладов.")
+
+        else:
+            break
+        0
+    print("\nСпасибо, что воспользовались терминалом банка! До встречи!")
+
+# -------------
+# Пример вывода (файл):
+#
+# Добро пожаловать в систему подбора вкладов!
+#
+# -----
+# Нажмите 1, чтобы подобрать вклад, или что угодно для выхода.
+# 1
+# 1/2: Введите начальную сумму вклада: 1000
+# 2/2: Введите срок вклада (мес.): 12
+# Вклад              | Прибыль       | Итоговая сумма
+# Сохраняй           |    50.00 руб. | 1,050.00 руб.
+# Бонусный           |    50.00 руб. | 1,050.00 руб.
+# С капитализацией   |    51.16 руб. | 1,051.16 руб.
