3f1ae1aefce38f4733956e3c0bef23ab49f39e92
Hi-Tech-Mechanic <157356548+Hi-Tech-Mechanic@users.noreply.github.com>
2024-01-28 22:22:54 +0300

    Add files via upload

---
+++ Lab5/DateTime_Collection.py
@@ -0,0 +1,39 @@
+﻿# Программирование на языке высокого уровня (Python).
+# Задание №5 Вариант !!!
+#
+# Выполнил: Минатуллаев Р.Ш.
+# Группа: ПИН-б-з-21-1
+# E-mail: ruslan.minatullaev-1000@yandex.ru
+
+import json
+from DateTime_Methods import DateTime
+
+class Collection:
+    def __init__(self):
+        self.__data = []
+
+    def __getitem__(self):
+        pass
+
+    def add(self, value):
+        self.__data.append(value)
+    
+    def remove(self, index):
+        self.__data.pop(index)
+
+    def save(self):
+        data = {"collection": self.get_data()}
+        with open("data.json", "w") as file:
+            json.dump(json.dumps(data, default=class_to_dict), file)
+        print("Данные сохранены: {}".format(self.get_data()))
+
+    def load(self):
+        with open("data.json", "r") as file:
+            json.load(file)
+        print("Данные загружены: {}".format(self.get_data()))
+
+    def get_data(self):
+        return [component for component in self.__data]
+
+def class_to_dict(obj):
+    return obj.__dict__
--- Lab5/main.py
+++ Lab5/main.py
@@ -1,1 +1,28 @@
+﻿# Программирование на языке высокого уровня (Python).
+# Задание №5 Вариант !!!
+#
+# Выполнил: Минатуллаев Р.Ш.
+# Группа: ПИН-б-з-21-1
+# E-mail: ruslan.minatullaev-1000@yandex.ru
+
+from DateTime_Collection import Collection 
+from DateTime_Methods import DateTime
+
+if __name__ == "__main__":
+    tmp = DateTime()
+    new_collection = Collection()
+
+    new_collection.add(tmp)
+    new_collection.save()
+    new_collection.load()
+    print(new_collection.get_data())
+    new_collection.remove(0)
+    print(new_collection.get_data())
+
+    ############################################
+    for i in range(0, 10):
+        new_collection.add(tmp)
+        new_collection.save()
+        new_collection.load()
+        print(new_collection.get_data())
 
